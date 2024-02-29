a3f7229c38bd38302f506cf0f4e8f533baa39245
Devansh Vishwakarma <devwish0207@gmail.com>
2024-02-29 15:35:46 +0530

    feat: Testomonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 import Scroll  from "@/components/Scroll";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import { Testomonial } from "@/components/Testomonial";
+
 export default function Home() {
   return (
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection/>
         <FeaturedCourses/>
-        <Scroll/>
+        {/* <Scroll/> */}
+        <Testomonial/>
       </main>
   );
 }
